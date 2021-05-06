509ec034a9d59173cbfd007271a4937f07532af5
Shaun Chiarolla <schiarolla@gmail.com>
2021-05-06 11:56:44 -0700

    removed files for release

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
 import dotenv from 'dotenv'; dotenv.config()
-import { io, server, get_data, start_scan, power_on } from './lib'
+import { io, server, get_data, start_scan } from './lib'
 
 const { EXPRESS_PORT } = process.env
 
 start_scan()
 
 io.on('connection', socket => {
-
     socket.emit('database', get_data())
-
-    socket.on('poweron', server => {
-        power_on(server).then(start_scan).then(() => socket.emit('database', get_data()))
-    })
-
-    socket.on('poweroff', server => {
-        console.log('poweroff', server)
-    })
 })
 
 server.listen(EXPRESS_PORT)
