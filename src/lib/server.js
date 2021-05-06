55349656e73fd0e9384b125e0247e5b812363a85
Shaun Chiarolla <schiarolla@gmail.com>
2021-05-06 16:20:39 -0700

    fixed cors issues

--- src/lib/server.js
+++ src/lib/server.js
@@ -2,6 +2,6 @@ import http from 'http'
 import socket from 'socket.io'
 
 const server = http.createServer()
-const io = new socket.Server(server)
+const io = new socket.Server(server, { cors: { origin: '*' } })
 
 export { io, server }
